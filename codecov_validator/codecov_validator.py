225512aa75280ddf2a8e038f61c601a64962d275
mashi <59000162+mashi@users.noreply.github.com>
2021-03-19 21:17:42 -0300

    Add __name__.

--- codecov_validator/codecov_validator.py
+++ codecov_validator/codecov_validator.py
@@ -1,9 +1,10 @@
 import requests
 
 
-file = open("codecov.yml", "rb").read()
-received = requests.post("https://codecov.io/validate", data=file)
-message = received.content.decode("utf-8")
-if "Valid!" not in message:
-    print(message)
-    exit(1)
+if __name__ == "__main__":
+    file = open("codecov.yml", "rb").read()
+    received = requests.post("https://codecov.io/validate", data=file)
+    message = received.content.decode("utf-8")
+    if "Valid!" not in message:
+        print(message)
+        exit(1)
