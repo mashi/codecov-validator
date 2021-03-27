14b3087c789f6887e902ce66db4cdc62952e40e3
mashi <59000162+mashi@users.noreply.github.com>
2021-03-26 23:54:41 -0300

    Add unittests. (#6)

--- codecov_validator/ccv.py
+++ codecov_validator/ccv.py
@@ -1,14 +1,78 @@
+import click
 import requests
 
 
-def run_request():
-    file = open("codecov.yml", "rb").read()
-    received = requests.post("https://codecov.io/validate", data=file)
-    message = received.content.decode("utf-8")
-    if "Valid!" not in message:
-        print(message)
+@click.command()
+@click.option(
+    "--filename", default="codecov.yml", help="Codecov configuration file."
+)
+def ccv(filename):
+    file = open_file(filename)
+    result = run_request(file)
+    check_valid(result)
+
+
+def check_valid(result):
+    """
+    Check if the message contains the "Valid!" string
+    from the request call.
+    The exit(1) is used to indicate an error in the pre-commit.
+
+    Args:
+        result (str): message to be analyzed.
+    """
+    if "Valid!" in result:
+        print("Valid!")
+        exit(0)
+    else:
+        print(result)
         exit(1)
 
 
+def open_file(filename):
+    """
+    Tries to open the configuration file.
+
+    Args:
+        filename (str): name of the configuration file.
+
+    Returns:
+        bytes: contents of the configuration file, or
+            the string zero.
+    """
+    try:
+        with open(filename, "rb") as myconfig:
+            file = myconfig.read()
+        return file
+    except FileNotFoundError:
+        print("Configuration file not found.")
+        exit(1)
+
+
+def run_request(file):
+    """
+    Send the configuration to the codecov site.
+
+    Args:
+        file (string): contents of the configuration file.
+
+    Returns:
+        str: Result of the request.
+    """
+    try:
+        received = requests.post("https://codecov.io/validate", data=file)
+    except (
+        requests.exceptions.ConnectTimeout,
+        requests.exceptions.HTTPError,
+        requests.exceptions.ReadTimeout,
+        requests.exceptions.Timeout,
+        requests.exceptions.ConnectionError,
+    ):
+        print("Failed to establish connection. Check your internet.")
+        exit(1)
+    message = received.content.decode("utf-8")
+    return message
+
+
 if __name__ == "__main__":
-    run_request()
+    ccv()
